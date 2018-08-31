5e9c9c47cc5f1f157e553fea4c89d670e13a0a83
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-08-31 09:36:34 -0400

    add redux dependency
    Added redux for global state management

--- src/components/palette.js
+++ src/components/palette.js
@@ -1,74 +1,131 @@
-import React , {Fragment} from 'react';
-import {Color} from './color';
+import React, { Fragment } from "react";
+import { Color } from "./color";
+import * as R from "ramda";
 
 export class Palette extends React.Component {
+  constructor(props) {
+    super(props);
+    this.palette = React.createRef();
+  }
 
-    constructor(props) {
-        super(props);
-        this.palette = React.createRef();
+  state = {
+    colors: []
+  };
+
+  getRandomColor = (locked, color) => {
+    if (locked) {
+      return color;
     }
-
-    state = {
-        colors: []
-    };
-
-    getRandomColor = (locked, color) => {
-        if (locked) {
-            return color;
-        }
-        const letters = '0123456789ABCDEF';
-        let newColor = '#';
-        for (let i = 0; i < 6; i++) {
-            newColor += letters[Math.floor(Math.random() * 16)];
-        }
-        return newColor;
-    };
-
-    generate() {
-        let colors = [];
-        for (let i=1; i<=5; i++) {
-            colors.push({color: this.getRandomColor(), locked: false});
-            this.setState({colors});
-        }
-        const {history} = this.props;
-        history.push("/app/"+colors.map(color => (color.color.replace("#",""))).join("-"))
+    const letters = "0123456789ABCDEF";
+    let newColor = "#";
+    for (let i = 0; i < 6; i++) {
+      newColor += letters[Math.floor(Math.random() * 16)];
     }
+    return newColor;
+  };
 
+  toggleLock = index => e => {
+    console.log(index);
+    let newColors = this.state.colors;
+    newColors = R.update(
+      index,
+      { color: newColors[index].color, locked: !newColors[index].locked },
+      newColors
+    );
+    this.setState({ colors: newColors });
+    console.log(newColors);
+    // let newColor = this.state.colors[index];
+    // newColors = R.update(index, newColor, newColors);
+    // this.setState({colors: newColors});
+  };
 
-    componentDidMount() {
-        const {colors} = this.state;
-        for (let i=1; i<=5; i++) {
-            colors.push({color: this.getRandomColor(), locked: false});
-            this.setState({colors});
-        }
-        const {history} = this.props;
-        history.push("/app/"+colors.map(color => (color.color.replace("#",""))).join("-"))
+  generateNew() {
+    let colors = [];
+    for (let i = 1; i <= 5; i++) {
+      colors.push({ color: this.getRandomColor(), locked: false });
+      this.setState({ colors });
     }
+    const { history } = this.props;
+    history.push(
+      "/app/" + colors.map(color => color.color.replace("#", "")).join("-")
+    );
+  }
 
-    render() {
-
-        console.log(this.props.match.params);
-
-        const {state: {colors}} = this;
-        return (
-            <Fragment>
-                <header className="App-header">
-                    <button onClick={()=>{this.palette.current.focus()}}>Hey</button>
-                    <input
-                        value=""
-                        type="text"
-                        onKeyPress={(e) => this.generate() }
-                        ref={this.palette}
-                    />
-                    <h1 className="App-title">Randour</h1>
-                    <h6>Random color generator written in React</h6>
-                </header>
-                <div className="row">
-                    {colors.map((color, index) => (
-                        <Color background={color.color} key={color.color + index} />
-                    ))}
-                </div>
-            </Fragment>
-        );
+  generate() {
+    if (colors.length === 5) {
+      let colors = this.state.colors;
+      console.log(colors);
+      let newColors = [];
+      for (let i = 1; i <= 5; i++) {
+        newColors.push({
+          color: this.getRandomColor(colors[i].locked, colors[i]),
+          locked: colors[i].locked
+        });
+        this.setState({ colors: newColors });
+      }
+    } else {
+      this.generateNew();
     }
+  }
+
+  componentDidMount() {
+    const { colors } = this.state;
+    for (let i = 1; i <= 5; i++) {
+      colors.push({ color: this.getRandomColor(), locked: false });
+      this.setState({ colors });
+    }
+    const { history } = this.props;
+    history.push(
+      "/app/" + colors.map(color => color.color.replace("#", "")).join("-")
+    );
+  }
+
+  render() {
+    console.log(this.props.match.params);
+
+    const {
+      state: { colors }
+    } = this;
+    return (
+      <Fragment>
+        <header className="App-header">
+          <button
+            onClick={() => {
+              this.palette.current.focus();
+            }}
+          >
+            Hey
+          </button>
+          <input
+            value=""
+            type="text"
+            onKeyPress={e => this.generateNew()}
+            ref={this.palette}
+          />
+          <h1 className="App-title">
+            {"Randour".split("").map((char, index) => {
+              let color = this.getRandomColor();
+              return (
+                <span className="mono" style={{ color: color }} key={index}>
+                  {char}
+                </span>
+              );
+            })}
+          </h1>
+          <h6>Random color generator written in React</h6>
+        </header>
+        <div className="row">
+          {colors.map((color, index) => (
+            <Color
+              background={color.color}
+              key={color.color + index}
+              index={index}
+              isLocked={color.locked}
+              clickHandler={this.toggleLock}
+            />
+          ))}
+        </div>
+      </Fragment>
+    );
+  }
 }
--- src/components/reorder.js
+++ src/components/reorder.js
@@ -1,80 +1,104 @@
-// Forked from: https://github.com/chenglou/react-motion/tree/master/demos/demo8-draggable-list
-// Original: http://framerjs.com/examples/preview/#list-sorting.framer
+import React from "react";
+import ReactDOM from "react-dom";
+import { Spring } from "react-spring";
+import { Gesture } from "react-with-gesture";
+import range from "lodash/range";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Spring } from 'react-spring'
-import { Gesture } from 'react-with-gesture'
-import range from 'lodash/range'
-import './styles.css'
-
-const clamp = (n, min, max) => Math.max(Math.min(n, max), min)
-const itemsCount = 4
+const clamp = (n, min, max) => Math.max(Math.min(n, max), min);
+const itemsCount = 4;
 function reinsert(arr, from, to) {
-    const _arr = arr.slice(0)
-    const val = _arr[from]
-    _arr.splice(from, 1)
-    _arr.splice(to, 0, val)
-    return _arr
+  const _arr = arr.slice(0);
+  const val = _arr[from];
+  _arr.splice(from, 1);
+  _arr.splice(to, 0, val);
+  return _arr;
 }
 
 class App extends React.Component {
-    state = { mouseY: 0, topDeltaY: 0, isPressed: false, originalPosOfLastPressed: 0, order: range(itemsCount) }
+  state = {
+    mouseY: 0,
+    topDeltaY: 0,
+    isPressed: false,
+    originalPosOfLastPressed: 0,
+    order: range(itemsCount)
+  };
 
-    componentDidMount() {
-        window.addEventListener('touchmove', this.handleTouchMove)
-        window.addEventListener('touchend', this.handleMouseUp)
-        window.addEventListener('mousemove', this.handleMouseMove)
-        window.addEventListener('mouseup', this.handleMouseUp)
-    }
+  componentDidMount() {
+    window.addEventListener("touchmove", this.handleTouchMove);
+    window.addEventListener("touchend", this.handleMouseUp);
+    window.addEventListener("mousemove", this.handleMouseMove);
+    window.addEventListener("mouseup", this.handleMouseUp);
+  }
 
-    handleTouchStart = (key, pressLocation, e) => this.handleMouseDown(key, pressLocation, e.touches[0])
-    handleTouchMove = e => e.preventDefault() || this.handleMouseMove(e.touches[0])
-    handleMouseUp = () => this.setState({ isPressed: false, topDeltaY: 0 })
-    handleMouseDown = (pos, pressY, { pageY }) =>
-        this.setState({ topDeltaY: pageY - pressY, mouseY: pressY, isPressed: true, originalPosOfLastPressed: pos })
-    handleMouseMove = ({ pageY }) => {
-        const { isPressed, topDeltaY, order, originalPosOfLastPressed } = this.state
-        if (isPressed) {
-            const mouseY = pageY - topDeltaY
-            const currentRow = clamp(Math.round(mouseY / 100), 0, itemsCount - 1)
-            let newOrder = order
-            if (currentRow !== order.indexOf(originalPosOfLastPressed))
-                newOrder = reinsert(order, order.indexOf(originalPosOfLastPressed), currentRow)
-            this.setState({ mouseY: mouseY, order: newOrder })
-        }
+  handleTouchStart = (key, pressLocation, e) =>
+    this.handleMouseDown(key, pressLocation, e.touches[0]);
+  handleTouchMove = e =>
+    e.preventDefault() || this.handleMouseMove(e.touches[0]);
+  handleMouseUp = () => this.setState({ isPressed: false, topDeltaY: 0 });
+  handleMouseDown = (pos, pressY, { pageY }) =>
+    this.setState({
+      topDeltaY: pageY - pressY,
+      mouseY: pressY,
+      isPressed: true,
+      originalPosOfLastPressed: pos
+    });
+  handleMouseMove = ({ pageY }) => {
+    const {
+      isPressed,
+      topDeltaY,
+      order,
+      originalPosOfLastPressed
+    } = this.state;
+    if (isPressed) {
+      const mouseY = pageY - topDeltaY;
+      const currentRow = clamp(Math.round(mouseY / 100), 0, itemsCount - 1);
+      let newOrder = order;
+      if (currentRow !== order.indexOf(originalPosOfLastPressed))
+        newOrder = reinsert(
+          order,
+          order.indexOf(originalPosOfLastPressed),
+          currentRow
+        );
+      this.setState({ mouseY: mouseY, order: newOrder });
     }
+  };
 
-    render() {
-        const { mouseY, isPressed, originalPosOfLastPressed, order } = this.state
-        return (
-            <div className="demo8">
-                {range(itemsCount).map(i => {
-                    const active = originalPosOfLastPressed === i && isPressed
-                    const style = active
-                        ? { scale: 1.1, shadow: 16, y: mouseY }
-                        : { scale: 1, shadow: 1, y: order.indexOf(i) * 100 }
-                    return (
-                        <Spring immediate={name => active && name === 'y'} to={style} key={i}>
-                            {({ scale, shadow, y }) => (
-                                <div
-                                    onMouseDown={this.handleMouseDown.bind(null, i, y)}
-                                    onTouchStart={this.handleTouchStart.bind(null, i, y)}
-                                    className="demo8-item"
-                                    style={{
-                                        boxShadow: `rgba(0, 0, 0, 0.2) 0px ${shadow}px ${2 * shadow}px 0px`,
-                                        transform: `translate3d(${y}px,0,  10px) scale(${scale})`,
-                                        zIndex: i === originalPosOfLastPressed ? 99 : i
-                                    }}>
-                                    {order.indexOf(i) + 1}
-                                </div>
-                            )}
-                        </Spring>
-                    )
-                })}
-            </div>
-        )
-    }
+  render() {
+    const { mouseY, isPressed, originalPosOfLastPressed, order } = this.state;
+    return (
+      <div className="demo8">
+        {range(itemsCount).map(i => {
+          const active = originalPosOfLastPressed === i && isPressed;
+          const style = active
+            ? { scale: 1.1, shadow: 16, y: mouseY }
+            : { scale: 1, shadow: 1, y: order.indexOf(i) * 100 };
+          return (
+            <Spring
+              immediate={name => active && name === "y"}
+              to={style}
+              key={i}
+            >
+              {({ scale, shadow, y }) => (
+                <div
+                  onMouseDown={this.handleMouseDown.bind(null, i, y)}
+                  onTouchStart={this.handleTouchStart.bind(null, i, y)}
+                  className="demo8-item"
+                  style={{
+                    boxShadow: `rgba(0, 0, 0, 0.2) 0px ${shadow}px ${2 *
+                      shadow}px 0px`,
+                    transform: `translate3d(${y}px,0,  10px) scale(${scale})`,
+                    zIndex: i === originalPosOfLastPressed ? 99 : i
+                  }}
+                >
+                  {order.indexOf(i) + 1}
+                </div>
+              )}
+            </Spring>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
